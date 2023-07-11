8d42327e50ca9290d21cfa05bae7f83e8b17134a
grigoriypobeditel <newgrigory@yandex.ru>
2023-07-11 03:18:06 +0300

    update

--- source/api/auth/user.py
+++ source/api/auth/user.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 
-from source.databse.base import User
+from source.databse.base import session, select, User, UserRole
+
 
 
 class AuthUser(BaseModel):
@@ -9,4 +10,10 @@ class AuthUser(BaseModel):
 
 
 def route(data: AuthUser) -> dict:
-    return {}
+    stmt = select(User).where(User.email ==  data.email)
+    result = session.scalars(statement=stmt).first()
+    if not result:
+        return {"status": "error", "msg": "user not found"}
+    if result.password != data.password:
+        return {"status": "error", "msg": "wrong password"}
+    return {"status": "succsess", "msg": "authorization succsess"}
